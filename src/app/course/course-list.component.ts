9ab7cd945b63641a5ef84f75e61de97241e23d16
Siraj Gadhia <Siraj6514310786@Gmail.com>
2017-03-20 22:07:46 -0500

    Added edit/add functinality

--- src/app/course/course-list.component.ts
+++ src/app/course/course-list.component.ts
@@ -19,34 +19,80 @@ import {
 })
 export class CourseListComponent implements OnInit {
 
-  public timeLineList: any[]
+  public isNewCourse: boolean = false;
   public SchoolData: any = {
     "Courses": [],
     "Students": []
   };
-
+  public updateCourse: any = {
+    "$id": "",
+    "CourseID": 0,
+    "Name": "",
+    "Code": "",
+    "Description": ""
+  }
+  @ViewChild('editModal') public editModal: ModalDirective;
   constructor(private dataService: DataService) {
 
   }
 
-  getCourseName(courseID): String
-{ 
-   let courseName:string = "Till not enrolled in any course!";
-       for (let i = 0; i < this.SchoolData.Courses.length; i++) {
-            if (this.SchoolData.Courses[i].CourseID == courseID)
-            {
-                  courseName = this.SchoolData.Courses[i].Name ;
-                  break;
-            }
+  getCourseName(courseID): String {
+    let courseName: string = "Till not enrolled in any course!";
+    for (let i = 0; i < this.SchoolData.Courses.length; i++) {
+      if (this.SchoolData.Courses[i].CourseID == courseID) {
+        courseName = this.SchoolData.Courses[i].Name;
+        break;
+      }
     }
-  return courseName
-}
+    return courseName
+  }
 
- getData() {
+  saveChanges() {
+    //Simple way to add/update
+    if (this.isNewCourse) {
+      debugger
+      // here id takeng as random
+      this.updateCourse.CourseID = Math.floor((Math.random() * 99999) + 1);
+      this.SchoolData.Courses.unshift(this.updateCourse); // adding at 1st so easily visible
+    } 
+    else {
+      for (let i = 0; i < this.SchoolData.Courses.length; i++) 
+      {
+        if (this.SchoolData.Courses[i].CourseID == this.updateCourse.CourseID) 
+        {
+           this.SchoolData.Courses[i] = this.updateCourse;
+           break;
+        }
+      }
+      
+    }
+    this.editModal.hide();
+  }
+
+  editCourse(course ? : any) {
+    if (course) {
+      this.updateCourse = Object.assign({}, course);
+      this.isNewCourse = false;
+    } else {
+      this.updateCourse = {
+        "$id": "",
+        "CourseID": 0,
+        "Name": "",
+        "Code": "",
+        "Description": ""
+      }
+      this.isNewCourse = true;
+    }
+
+    this.editModal.show();
+  }
+
+
+
+  getData() {
 
     this.dataService.getSchoolData()
       .then((School_data: any) => {
-        debugger
         this.SchoolData = School_data;
       });
 
--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -1,8 +1,11 @@
 import {
   Component,
-  OnInit, ViewChild
+  OnInit,
+  ViewChild
 } from '@angular/core';
-import { ModalDirective } from 'ng2-bootstrap/modal';
+import {
+  ModalDirective
+} from 'ng2-bootstrap/modal';
 import {
   DataService
 } from '../services/data.service';
@@ -13,64 +16,100 @@ import {
   providers: [DataService]
 })
 export class StudentComponent implements OnInit {
-
-  public seachText: string = "";
-  public isShowSearchDD: boolean = false;
+  public isNewStudent: boolean = false;
   public SchoolData: any = {
     "Courses": [],
     "Students": []
   };
   public updateStudent: any = {
-      "$id": "",
-      "StudentID": 0,
-      "Year": "",
-      "Name": "",
-      "CourseID": -1,
-      "Grade": ""
+    "$id": "",
+    "StudentID": 0,
+    "Year": "",
+    "Name": "",
+    "CourseID": -1,
+    "Grade": ""
+  }
+
+  public grades = ['A++', 'A+', 'A', 'B+', 'B', 'C+', 'C', 'D+', 'D'];
+
+  @ViewChild('scModal') public scModal: ModalDirective;
+  @ViewChild('editModal') public editModal: ModalDirective;
+
+
+  constructor(private dataService: DataService) {}
+
+  saveChanges() {
+    //Simple way to add/update
+    if (this.isNewStudent) 
+    {
+      debugger
+      // here id takeng as random
+      this.updateStudent.StudentID = Math.floor((Math.random() * 100000) + 1);
+      this.SchoolData.Students.unshift(this.updateStudent); // adding at 1st so easily visible
+    } else 
+    {
+      for (let i = 0; i < this.SchoolData.Students.length; i++) {
+        if (this.SchoolData.Students[i].StudentID == this.updateStudent.StudentID) {
+          this.SchoolData.Students[i] = this.updateStudent;
+          break;
+       }
+      }
+      
+    }
+    this.editModal.hide();
+  }
+
+  editStudent(student ? : any) {
+    if (student) {
+      this.updateStudent = Object.assign({}, student);
+      this.isNewStudent = false;
+    } else {
+      this.updateStudent = {
+        "$id": "",
+        "StudentID": 0,
+        "Year": "",
+        "Name": "",
+        "CourseID": -1,
+        "Grade": ""
+      }
+      this.isNewStudent = true;
     }
 
-@ViewChild('scModal') public scModal:ModalDirective;
+    this.editModal.show();
+  }
 
-constructor(private dataService: DataService) {}
-  
-getCourseName(courseID): String
-{ 
-   let courseName:string = "Till not enrolled in any course!";
-       for (let i = 0; i < this.SchoolData.Courses.length; i++) {
-            if (this.SchoolData.Courses[i].CourseID == courseID)
-            {
-                  courseName = this.SchoolData.Courses[i].Name ;
-                  break;
-            }
+  getCourseName(courseID): String {
+    let courseName: string = "Till not enrolled in any course!";
+    for (let i = 0; i < this.SchoolData.Courses.length; i++) {
+      if (this.SchoolData.Courses[i].CourseID == courseID) {
+        courseName = this.SchoolData.Courses[i].Name;
+        break;
+      }
     }
-  return courseName
-}
+    return courseName
+  }
 
-replaceCourse(CourseID)
-{
+  replaceCourse(CourseID) {
     for (let i = 0; i < this.SchoolData.Students.length; i++) {
-            if (this.SchoolData.Students[i].StudentID == this.updateStudent.StudentID)
-            {
-                  this.SchoolData.Students[i].CourseID = CourseID ;
-                  this.scModal.hide();
-                  break;
-            }
+      if (this.SchoolData.Students[i].StudentID == this.updateStudent.StudentID) {
+        this.SchoolData.Students[i].CourseID = CourseID;
+        this.scModal.hide();
+        break;
+      }
     }
-}
+  }
 
-updateCourse(student)
-{  
-     //debugger; 
-     this.updateStudent = student;
+  updateCourse(student) {
+    //debugger; 
+    this.updateStudent = student;
     this.scModal.show();
-}
+  }
   removeCourse(studentID) {
     for (let i = 0; i < this.SchoolData.Students.length; i++) {
-            if (this.SchoolData.Students[i].StudentID == studentID)
-            {
-                  this.SchoolData.Students[i].CourseID = -1 ;
-                  break;
-            }
+      if (this.SchoolData.Students[i].StudentID == studentID) {
+        this.SchoolData.Students[i].CourseID = -1;
+        break;
+      }
     }
   }
 
